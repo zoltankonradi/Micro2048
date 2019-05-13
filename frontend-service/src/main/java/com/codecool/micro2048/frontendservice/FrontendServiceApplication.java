e2570811ed0aabb740a5f5d99b22d2f97f84399a
Zoli <zoltan.konradi@gmail.com>
2019-05-13 14:49:30 +0200

    Frontend branch created.

---
+++ frontend-service/src/main/java/com/codecool/micro2048/frontendservice/FrontendServiceApplication.java
@@ -0,0 +1,17 @@
+package com.codecool.micro2048.frontendservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class FrontendServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrontendServiceApplication.class, args);
+    }
+
+
+
+}
---
+++ frontend-service/src/main/java/com/codecool/micro2048/frontendservice/controller/FrontEndController.java
@@ -0,0 +1,10 @@
+package com.codecool.micro2048.frontendservice.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FrontEndController {
+
+
+
+}
