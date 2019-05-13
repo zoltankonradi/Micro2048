8bdffdbe3ee3f37d0164a5cc99043bf3d2ccff9c
Zoli <zoltan.konradi@gmail.com>
2019-05-13 14:10:47 +0200

    mvn-wrapper.jar added to gitignore

--- api-gateway/src/main/java/com/codecool/micro2048/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/codecool/micro2048/apigateway/ApiGatewayApplication.java
@@ -10,7 +10,6 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @EnableZuulProxy
 public class ApiGatewayApplication {
 
-
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
