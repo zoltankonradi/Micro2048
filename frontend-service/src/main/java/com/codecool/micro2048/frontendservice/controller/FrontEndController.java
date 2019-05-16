5af6d899741503e44ef9f4ecdb2f5dbea00637c9
Zoli <zoltan.konradi@gmail.com>
2019-05-16 09:04:49 +0200

    Fixed quote text so that it starts with an actual quote on page load.

--- frontend-service/src/main/java/com/codecool/micro2048/frontendservice/controller/FrontEndController.java
+++ frontend-service/src/main/java/com/codecool/micro2048/frontendservice/controller/FrontEndController.java
@@ -1,13 +1,19 @@
 package com.codecool.micro2048.frontendservice.controller;
 
+import com.codecool.micro2048.frontendservice.controller.service.QuoteService;
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FrontEndController {
 
     @GetMapping(value = "/")
-    public String getGame() {
+    public String getGame(Model model) {
+        QuoteService quoteService = new QuoteService();
+        JSONObject json = new JSONObject(quoteService.getQuote());
+        model.addAttribute("quote", json.getString("quote"));
         return "game";
     }
 
